9f01e5b8bb73a1ccbcd0639d2373b3ce72f4906c
Zipnx <55202028+Zipnx@users.noreply.github.com>
2025-08-27 22:05:53 +0300

    Some raw apuf stuff i had forgotten to add

---
+++ standalone/ps_rawapuf/firmware/rawapuf_interact/src/main.c
@@ -0,0 +1,52 @@
+
+/*
+ * main.c
+ *
+ *  Created on: Aug 18, 2025
+ *      Author: Zipnx
+ */
+
+#include "xparameters.h"
+#include "hcm.h"
+
+int main(void) {
+	HCM* module;
+	HCMSTATUS result;
+
+	// idc about the result
+	result = HCM_EnableDebugCon(XPAR_PS7_UART_0_DEVICE_ID);
+	result = HCM_Init(&module, XPAR_PS7_UART_1_DEVICE_ID);
+
+	INFO("Init Result: %08x", result);
+
+	if (result != HCMSUCCESS)
+		return XST_FAILURE;
+
+	INFO("Init complete");
+
+	HCM_EnableXSysmon(module, XPAR_SYSMON_0_DEVICE_ID);
+	HCM_EnableRawAPUF(module, XPAR_APUF_RAW_AXI_V1_0_0_BASEADDR);
+
+	// TODO: Make an error for this, if a read happens with rx disabled
+	HCM_SetRxEnabled(module, true);
+	HCM_UnlockAll(module);
+
+	Command cmd;
+	Response resp;
+
+	while (1) {
+		INFO("Receiving...");
+		result = HCM_CommandReceive(module, &cmd);
+
+		if (result != HCMPASS) continue;
+
+		INFO("Recv result: 0x%08x", result);
+
+		HCM_ResponseMake(module, &resp);
+		resp.size = 16;
+		HCM_ResponseSend(module, &resp);
+
+		OKAY("Sent response.");
+		break;
+	}
+}
